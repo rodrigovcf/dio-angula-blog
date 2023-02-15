3135e5d5b0a1078bb74fe9d274c7006fa9dbc26e
Rodrigo Valença <rodrigovalenca@MacBook-Pro-de-Rodrigo.local>
2023-02-15 11:51:43 -0300

    Created content component

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -16,6 +16,9 @@ export class BigCardComponent implements OnInit{
   @Input()
   cardDescriptino:string = ""
 
+  @Input()
+  Id:string = "0"
+
   constructor(){}
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Marvel - Vingadores 2023",
+    "description": "A Marvel anuncia novo ator para substituir o personagem Homem de Ferro. Um novo filme está em produção para lançamento em 2025.",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Séries Marvel são sucesso na Disney Plus",
+    "description": "Os sucessos dos cinemas produzidos pelo universo Marvel, se tornaram séries na Disney plus",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Novo filme do Pantera Negra.",
+    "description": "Marvel acaba de lançar o novo filme da Pantera Negra",
+    "photoCover": "https://midias.correiobraziliense.com.br/_midias/jpg/2022/11/09/675x450/1_pantera_negra_wakanda_para_sempre-26819084.jpg?20221109143843?20221109143843"
+  },
+  {
+    "id":"4",
+    "title": "Novo filme do Hulk.",
+    "description": "Marvel acaba de lançar o novo filme do Hulk",
+    "photoCover": "https://blogger.googleusercontent.com/img/a/AVvXsEi9PtsP3-buZNeAk2dYrCrr3qb8IjE-9ixXXxbIFk4F1mMxNiPp3JTe2vX-tnn2umYbCQw9wsYyltd7eiPYBMip7jYyfHk939FDAW8cl17gCy4OrAKtgMT_vSJWx_fztdFaOvMcpO0RzrPfhfIer2L80FjgMTGs2-XFa7fV9cdWviGYMxYpz6zVl_04=s1600"
+  },
+]
